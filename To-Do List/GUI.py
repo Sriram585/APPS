d12acbaadbea1f62c4b03bae87bafa7afc8bcb21
Sriram <malladisriram273@gmail.com>
2025-01-26 21:15:57 +0530

    done with coding

--- To-Do List/GUI.py
+++ To-Do List/GUI.py
@@ -1,77 +1,66 @@
-import FreeSimpleGUI as fsg
+import FreeSimpleGUI as sg
 import Functions
 import time
+import os
 
-fsg.theme("DarkPurple4")
-clock=fsg.Text(time.strftime("%b %d,%Y"),key="time")
-label=fsg.Text("Type in your To-Do")
-input_box=fsg.InputText(tooltip="Enter To-do",key="todo")
-list_box=fsg.Listbox(values=Functions.get_todos(),size=(40,10),
-                      key="todos",enable_events=True)
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
-add_button=fsg.Button("Add",size=10)
-edit_button=fsg.Button("Edit",size=10)
-complete_button=fsg.Button("Complete",size=10)
-exit_button=fsg.Button("Exit",size=10)
+sg.theme("Black")
+clock = sg.Text(time.strftime("%b %d, %Y"), key="time")
+label = sg.Text("Type in your To-Do")
+input_box = sg.InputText(tooltip="Enter To-do", key="todo")
+list_box = sg.Listbox(values=Functions.get_todos(), size=(40, 10),
+                      key="todos", enable_events=True)
 
+add_button = sg.Button("Add", size=(10, 1))
+edit_button = sg.Button("Edit", size=(10, 1))
+complete_button = sg.Button("Completed", size=(10, 1))
+exit_button = sg.Button("Exit", size=(10, 1))
 
-window=fsg.Window("My To-Do APP",
-                  layout=[[clock],[label],[input_box,add_button],
-                  [list_box,edit_button,complete_button],
-                  [exit_button]],
-                  font=("Helvetica",20))
+window = sg.Window("My To-Do APP",
+                   layout=[[clock], [label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=("Helvetica", 20))
+
 try:
     while True:
-        event,values=window.read()
-        match event:
-            case "Add":
-                todos=Functions.get_todos()
-                new_todo=values["todo"]+"\n"
-                todos.append(new_todo)
+        event, values = window.read(timeout=100)
+        window["time"].update(time.strftime("%b %d, %Y"))
+        if event == sg.WINDOW_CLOSED or event == "Exit":
+            break
+        if event == "Add":
+            todos = Functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            Functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        elif event == "Edit":
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+                todos = Functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + "\n"
                 Functions.write_todos(todos)
                 window["todos"].update(values=todos)
-            # case "Edit":
-            #     try:
-            #         todo_to_edit=values["todos"][0]
-            #         print(type(todo_to_edit))
-            #         new_todo=values["todo"]
-            #         todos=Functions.get_todos()
-            #         print(todos)
-            #         index=todos.index(todo_to_edit)
-            #         todos[index]=new_todo
-            #         Functions.write_todos(todos)
-            #         window["todos"].update(values=todos)
-            #     except IndexError:
-            #         fsg.popup("please select an item first")
-            case "Edit":
-                try:
-                    todo_to_edit = values["todos"][0].strip("\n")
-                    new_todo = values["todo"]
-                    todos = [i.strip("\n") for i in Functions.get_todos()]
-                    print(todos)
-                    index = todos.index(todo_to_edit)
-                    todos[index] = new_todo+"\n"
-                    Functions.write_todos(todos)
-                    window["todos"].update(values=todos)
-                except IndexError:
-                    fsg.popup("Please select an item first")
-
-            case "todos":
-                window["todo"].update(value=values["todos"][0])
-            case "Complete":
-                try:
-                    todos=Functions.get_todos()
-                    todo_to_complete=values["todos"][0]
-                    index=todos.index(todo_to_complete)
-                    todos.pop(index)
-                    Functions.write_todos(todos)
-                    window["todos"].update(values=todos)
-                    window["todo"].update(value="")
-                except IndexError:
-                    fsg.popup("Please select an item first")
-            case fsg.WIN_CLOSED:
-                break
-            case "Exit":
-                break
+            except IndexError:
+                sg.popup("Please select an item first")
+        elif event == "todos":
+            window["todo"].update(value=values["todos"][0])
+        elif event == "Completed":
+            try:
+                todos = Functions.get_todos()
+                todo_to_complete = values["todos"][0]
+                index = todos.index(todo_to_complete)
+                todos.pop(index)
+                Functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first")
 finally:
     window.close()
+print("Thank you for using our APP")
