0df690900c5b87481f38e9fcca117f8d39cc736c
Sriram <malladisriram273@gmail.com>
2025-01-26 12:59:04 +0530

    added edit,complete and edit buttons

--- To-Do List/GUI.py
+++ To-Do List/GUI.py
@@ -3,15 +3,19 @@ import Functions
 
 label=fsg.Text("Type in your To-Do")
 input_box=fsg.InputText(tooltip="Enter To-do",key="todo")
-add_button=fsg.Button("Add")
 list_box=fsg.Listbox(values=Functions.get_todos(),size=(40,10),
                       key="todos",enable_events=True)
+
+add_button=fsg.Button("Add")
 edit_button=fsg.Button("Edit")
+complete_button=fsg.Button("complete")
+exit_button=fsg.Button("Exit")
 
 
 window=fsg.Window("My To-Do APP",
                   layout=[[label],[input_box,add_button],
-                  [list_box,edit_button]],
+                  [list_box,edit_button,complete_button],
+                  [exit_button]],
                   font=("Helvetica",20))
 while True:
     event,values=window.read()
@@ -34,6 +38,16 @@ while True:
             window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "complete":
+            todos=Functions.get_todos()
+            todo_to_complete=values["todos"][0]
+            index=todos.index(todo_to_complete)
+            todos.pop(index)
+            Functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "exit":
+            break
         case fsg.WIN_CLOSED:
             break
 window.close()
